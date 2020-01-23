66b0387d71f5a968143a1d19876c61c78249b529
zelenovakat <git@github.com:zelenovakat/github-demo.git>
2020-01-23 14:56:45 +0100

    Change app.js (title and ul)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title.jsx'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Title>
   Kate List
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-
-        >
-
+        </Title>
 <ul>
   <li>milk</li>
   <li>milk</li>
@@ -26,9 +17,6 @@ function App() {
   <li>milk</li>
   <li>milk</li>
 </ul>
-
-        </a>
-      </header>
     </div>
   );
 }
