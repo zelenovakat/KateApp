2b18021b8b6f35b7bfc3b1910888df0f24d5ebbe
zelenovakat <git@github.com:zelenovakat/github-demo.git>
2020-01-23 09:09:52 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+  Kate List
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+<ul>
+  milk
+</ul>
+<ul>
+  juice
+</ul>
+<ul>
+  agg
+</ul>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
