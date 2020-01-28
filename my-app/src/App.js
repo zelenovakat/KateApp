13d1f4a3dc78ea907e65bb4541071be2765ac202
zelenovakat <git@github.com:zelenovakat/github-demo.git>
2020-01-28 07:53:45 +0100

    change defolt product(object)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,37 +2,56 @@ import React, { useState } from 'react';
 import './App.css';
 import Title from './components/Title'
 import ProductsList from './components/ProductsList';
+import styled from 'styled-components'
+
+const Button = styled.button`
+margin-left: 20px;
+`
+const StyledDiv = styled.div`
+display: flex;
+justify-content: center;
+// max-width: 500px;
+`
+
 
 function App() {
   const [newProduct, setNewProduct] = useState('');
-  const defaultProducts = ["milk", "Juise", "agg"]
+  const defaultProducts = [
+    { name: 'milk', completed: false },
+    { name: 'juice', completed: false },
+    { name: 'eggs', completed: true },
+  ]
   const [products, setProducts] = useState(defaultProducts)
+
   const handleClick = () => {
-    const updatedProducts = [newProduct, ...products]
+    const newItem = { name: newProduct, completed: false }
+    const updatedProducts = [newItem, ...products]
     setProducts(updatedProducts)
     setNewProduct('')
   }
+
   return (
-    <>
-      <Title>
-        Kate List
-    </Title>
+    <StyledDiv>
       <form>
+        <Title>
+          Kate List
+    </Title>
         <label>
           New product:
        <input type="text" value={newProduct} onChange={(event) => {
             console.log(event.target.value)
             setNewProduct(event.target.value)
           }}
+
           />
-          <button type="button" onClick={handleClick}>Add
-       </button>
+          <Button type="button" onClick={handleClick} >Add
+       </Button>
           <ProductsList products={products} setProductList={setProducts} />
         </label>
       </form>
 
 
-    </>
+    </StyledDiv>
   )
 }
 
