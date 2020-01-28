24faeaab78a7957e1e3b1b96b1664c2f57aa5170
zelenovakat <git@github.com:zelenovakat/github-demo.git>
2020-01-28 07:55:37 +0100

    change state product+textDecoration

--- my-app/src/components/ProductsList.jsx
+++ my-app/src/components/ProductsList.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { updateObjectInArray, deleteObjectInArray } from "../utils/helper"
 
 const ProductsList = (props) => {
   const { products, setProductList } = props
   const handleClick = (i) => {
-    const updatedProducts = [...products.slice(0, i ), ...products.slice(i + 1)]
+    const updatedProducts = deleteObjectInArray(products, i)
     setProductList(updatedProducts)
   }
-  const mapedProducts = products.map((item, i) => {
 
+  const clickOnProduct = (i) => {
+    // 1. Create new element with updates
+    const oneItem = {...products[i], completed: !products[i].completed}
+    // 2. Prepare action for update
+    const action = {
+      index: i,
+      item: oneItem
+    }
+    // 3. Get new updated full products list
+    const updatedProducts = updateObjectInArray(products, action)
+    // 4. Set new state
+    setProductList(updatedProducts)
+  }
+
+
+  const mapedProducts = products.map(({ name, completed }, i) => {
+    console.log(name)
     return (
-      <li key={item}>
-        {item}
+      <li key={name}
+        style={{
+          textDecoration: completed ? 'line-through' : 'none'
+        }}>
+       <p onClick={() => {clickOnProduct(i) }
+         }>{name}</p>
         <button onClick={() => handleClick(i)}>
-        <FontAwesomeIcon icon={faTrashAlt} />
+          <FontAwesomeIcon icon={faTrashAlt} />
         </button>
       </li>
+
     )
   })
 
